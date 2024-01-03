40ba354eb94d2fd69e6db28cb290352e4db0e4a3
Rui <114363176+ruiichen@users.noreply.github.com>
2024-01-03 23:39:23 +0000

    test

--- src/main/java/com/wealthshrimple/WealthShrimple/WealthShrimpleApplication.java
+++ src/main/java/com/wealthshrimple/WealthShrimple/WealthShrimpleApplication.java
@@ -3,6 +3,7 @@ package com.wealthshrimple.WealthShrimple;
 import java.util.Arrays;
 import java.util.Collections;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
